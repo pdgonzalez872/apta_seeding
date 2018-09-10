90273089e72741d50da56c0d5ab5afe1f22b4402
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-09-10 16:12:34 -0500

    Nice, I like this better. Passing state around.

--- lib/apta_seeding/etl/season_data.ex
+++ lib/apta_seeding/etl/season_data.ex
@@ -8,25 +8,57 @@ defmodule AptaSeeding.ETL.SeasonData do
     root_url: "https://platformtennisonline.org"
     api_url: "/services/2015ServiceRanking.asmx/GetRanking"
     post_params: {"stype":2,"rtype":1,"sid":10,"rnum":0,"copt":3,"xid":0}
+
+  I came up with this after looking at how the website worked. I started with
+  their url and also looked at their JS. That's where the bulk of the fetching was done.
   """
   def call(tournament_params_to_post) do
     tournament_params_to_post
+    |> init()
     |> extract()
     |> transform()
     |> load()
   end
 
-  def extract(args) do
-    args
+  def init(args) do
+    {:ok, %{step: :initialized, args: args}}
   end
 
-  def transform(args) do
-    args
+  @doc """
+  We make the request to the third party api here.
+  """
+  def extract({:ok, state}) do
+    api_call_response = "yay response"
+
+    state = state
+            |> Map.put(:step, :extract)
+            |> Map.put(:api_call_response, api_call_response)
+
+    {:ok, state}
   end
 
-  def load(args) do
-    args
-    {:ok, args}
+  @doc """
+  We take in a response from the 3rd party server and
+  put it in a format we like.
+  """
+  def transform({:ok, state}) do
+    tournaments = []
+
+    state = state
+            |> Map.put(:step, :transform)
+            |> Map.put(:tournaments, tournaments)
+
+    {:ok, state}
+  end
+
+  @doc """
+  We don't do anything with the data, just pass it along.
+  """
+  def load({:ok, state}) do
+    state = state
+            |> Map.put(:step, :load)
+
+    {:ok, state}
   end
 
   @doc """
--- test/acceptance_test.exs
+++ test/acceptance_test.exs
@@ -8,15 +8,17 @@ defmodule AptaSeeding.AcceptanceTest do
 
       {:ok, result} = ETL.handle_season_data(["hahaha html right"])
 
-      assert Enum.at(result, 0) == %{third_party_tournament_id: "416",
-                                     tournament_date: "12/09/17",
-                                     tournament_name: "Western New England Men",
-                                     tournament_weight: "2.500"}
+      assert result == 1
 
-      assert Enum.at(result, -1) == %{third_party_tournament_id: "343",
-                                      tournament_date: "12/10/16",
-                                      tournament_name: "Detroit Invitational Men",
-                                      tournament_weight: "1.375"}
+      assert Enum.at(result.tournaments, 0) == %{third_party_tournament_id: "416",
+                                                 tournament_date: "12/09/17",
+                                                 tournament_name: "Western New England Men",
+                                                 tournament_weight: "2.500"}
+
+      assert Enum.at(result.tournaments, -1) == %{third_party_tournament_id: "343",
+                                                  tournament_date: "12/10/16",
+                                                  tournament_name: "Detroit Invitational Men",
+                                                  tournament_weight: "1.375"}
     end
   end
 end
