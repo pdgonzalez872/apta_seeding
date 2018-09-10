d7b813651759f6a05c335c64d630617a36a78f81
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-09-10 15:23:01 -0500

    wip: work on Extract context

--- lib/apta_seeding/etl/extract/tournaments_payload.ex
+++ lib/apta_seeding/etl/extract/tournaments_payload.ex
@@ -1,5 +1,22 @@
 defmodule AptaSeeding.ETL.Extract.TournamentsPayload do
-  def call() do
+
+  @doc """
+  This takes in a map that gets converted to json, but they are really post params
+  Then, we make a request to their api service:
+
+  Ex:
+    root_url: "https://platformtennisonline.org"
+    api_url: "/services/2015ServiceRanking.asmx/GetRanking"
+    post_params: {"stype":2,"rtype":1,"sid":10,"rnum":0,"copt":3,"xid":0}
+  """
+  def call(request_target) do
+    # request_target
+    # |> prepare_request
+    # |> make_request()
+    # |> parse_response_json()
+    # |> parse_html()
+    # |> prepare_output()
+
     :tournaments_payload
   end
 
--- test/acceptance_test.exs
+++ test/acceptance_test.exs
@@ -1,11 +1,23 @@
 defmodule AptaSeeding.AcceptanceTest do
   use ExUnit.Case
 
-  alias AptaSeeding.ETL
+  alias AptaSeeding.ETL.Extract
 
   describe "This is where we try things out" do
     test "main" do
-      assert ETL.handle_tournaments_payload() == 1
+      assert Extract.handle_tournaments_payload("hahaha html right") == 1
+
+      result = []
+
+      assert Enum.at(result, 0) == %{third_party_tournament_id: "416",
+                                     tournament_date: "12/09/17",
+                                     tournament_name: "Western New England Men",
+                                     tournament_weight: "2.500"}
+
+      assert Enum.at(result, -1) == %{third_party_tournament_id: "343",
+                                      tournament_date: "12/10/16",
+                                      tournament_name: "Detroit Invitational Men",
+                                      tournament_weight: "1.375"}
     end
   end
 end
