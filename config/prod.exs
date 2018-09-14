73f7f3e36ff82926760c25e55d036c08d2ecf8f8
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-09-14 12:25:36 -0500

    Fix typo in config

--- config/prod.exs
+++ config/prod.exs
@@ -7,7 +7,7 @@ config :apta_seeding, AptaSeedingWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-config :apta_seeding, AptaSeedingWeb.Repo,
+config :apta_seeding, AptaSeeding.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
