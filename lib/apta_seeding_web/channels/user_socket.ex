b69c86f2324bd126cfe5187e2e7d614193174ffa
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-09-14 12:18:32 -0500

    Work on config - phoenix docs

--- lib/apta_seeding_web/channels/user_socket.ex
+++ lib/apta_seeding_web/channels/user_socket.ex
@@ -6,7 +6,7 @@ defmodule AptaSeedingWeb.UserSocket do
 
   ## Transports
   transport(:websocket, Phoenix.Transports.WebSocket)
-  # transport :longpoll, Phoenix.Transports.LongPoll
+    timeout: 45_000
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
