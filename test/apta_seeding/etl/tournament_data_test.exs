d95221779515a24cf478f7a45226579e2c8d1e00
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-09-15 10:09:12 -0500

    Fix test

--- test/apta_seeding/etl/tournament_data_test.exs
+++ test/apta_seeding/etl/tournament_data_test.exs
@@ -6,15 +6,14 @@ defmodule AptaSeeding.ETL.TournamentDataTest do
   describe "TournamentData" do
     test "create_tournament_json_payload/1 creates the json payload correctly" do
       input = %{
-        :tournament_date => "03/06/15",
-        :tournament_name => "APTA Men's Nationals",
-        :xid => "214",
-        "copt" => 3,
-        "rnum" => 0,
-        "rtype" => 1,
-        "sid" => 8,
-        "stype" => 2,
-        "xid" => 0
+        tournament_date: "03/06/15",
+        tournament_name: "APTA Men's Nationals",
+        xid: "214",
+        copt: 3,
+        rnum: 0,
+        rtype: 1,
+        sid: 8,
+        stype: 2,
       }
 
       result = TournamentData.create_tournament_json_payload(input)
