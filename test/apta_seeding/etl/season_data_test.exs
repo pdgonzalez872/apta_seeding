17f8ba4561055f2de0c9597e71e4e76c2e7cd5e3
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-09-15 10:10:58 -0500

    Fix tests

--- test/apta_seeding/etl/season_data_test.exs
+++ test/apta_seeding/etl/season_data_test.exs
@@ -7,12 +7,12 @@ defmodule AptaSeeding.ETL.SeasonDataTest do
     test "create_season_url/1 returns the correct url 2015" do
       result =
         SeasonData.create_season_url(%{
-          "copt" => 3,
-          "rnum" => 0,
-          "rtype" => 1,
-          "sid" => 8,
-          "stype" => 2,
-          "xid" => 0
+          copt: 3,
+          rnum: 0,
+          rtype: 1,
+          sid: 8,
+          stype: 2,
+          xid: 0
         })
 
       expected = "https://platformtennisonline.org/Ranking.aspx?stype=2&rtype=1&sid=8&copt=3"
@@ -23,12 +23,12 @@ defmodule AptaSeeding.ETL.SeasonDataTest do
     test "create_season_url/1 returns the correct url 2016" do
       result =
         SeasonData.create_season_url(%{
-          "stype" => 2,
-          "rtype" => 1,
-          "sid" => 9,
-          "rnum" => 0,
-          "copt" => 3,
-          "xid" => 0
+          stype: 2,
+          rtype: 1,
+          sid: 9,
+          rnum: 0,
+          copt: 3,
+          xid: 0
         })
 
       expected = "https://platformtennisonline.org/Ranking.aspx?stype=2&rtype=1&sid=9&copt=3"
