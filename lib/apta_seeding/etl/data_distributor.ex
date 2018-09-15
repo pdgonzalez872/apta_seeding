091aee4d2bcc24f02cf928438cca95b2dd657c70
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-09-15 01:56:11 -0500

    Add distributor concept

---
+++ lib/apta_seeding/etl/data_distributor.ex
@@ -0,0 +1,13 @@
+defmodule AptaSeeding.ETL.DataDistributor do
+  @moduledoc """
+  Update tournament results_have_been_processed to true after processing a tournament
+
+    # Do the below in the DataDistributor
+    # create the data structure
+    # - Player
+    # - Team
+    # - Player Result (points, half of a team result)
+    # - Team Result (points)
+
+  """
+end
