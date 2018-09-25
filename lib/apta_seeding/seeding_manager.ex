6b2139ab4d832ae1f7d3b0d54a9284a0dc64c290
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-09-25 10:04:02 -0500

    Move things away from seeding manager

--- lib/apta_seeding/seeding_manager.ex
+++ lib/apta_seeding/seeding_manager.ex
@@ -39,8 +39,7 @@ defmodule AptaSeeding.SeedingManager do
   """
 
   alias AptaSeeding.Data
-  alias AptaSeeding.SeedingManager.SeasonManager
-  alias AptaSeeding.SeedingManager.SeedingReporter
+  alias AptaSeeding.SeedingManager.{SeasonManager, SeedingReporter, TournamentPicker}
 
   def call({:ok, state}) do
     {:ok, state}
@@ -111,7 +110,7 @@ defmodule AptaSeeding.SeedingManager do
   end
 
   def handle_seeding_criteria(tdo, :team_has_played_3_tournaments = seeding_criteria) do
-    team_results_details = get_team_points(tdo, seeding_criteria)
+    team_results_details = TournamentPicker.get_team_points(tdo, seeding_criteria)
 
     tdo
     |> Map.put(:seeding_criteria, seeding_criteria)
@@ -127,9 +126,9 @@ defmodule AptaSeeding.SeedingManager do
         tdo,
         :team_has_played_2_tournaments_1_best_individual = seeding_criteria
       ) do
-    team_results_details = get_team_points(tdo, seeding_criteria)
+    team_results_details = TournamentPicker.get_team_points(tdo, seeding_criteria)
 
-    individual_results_details = get_individual_points(tdo, seeding_criteria)
+    individual_results_details = TournamentPicker.get_individual_points(tdo, seeding_criteria)
 
     individual_total_points =
       individual_results_details
@@ -160,9 +159,9 @@ defmodule AptaSeeding.SeedingManager do
         tdo,
         :team_has_played_1_tournament_2_best_individual = seeding_criteria
       ) do
-    team_results_details = get_team_points(tdo, seeding_criteria)
+    team_results_details = TournamentPicker.get_team_points(tdo, seeding_criteria)
 
-    individual_results_details = get_individual_points(tdo, seeding_criteria)
+    individual_results_details = TournamentPicker.get_individual_points(tdo, seeding_criteria)
 
     individual_total_points =
       individual_results_details
@@ -190,7 +189,7 @@ defmodule AptaSeeding.SeedingManager do
         tdo,
         :team_has_not_played_together_3_best_individual = seeding_criteria
       ) do
-    individual_results_details = get_individual_points(tdo, seeding_criteria)
+    individual_results_details = TournamentPicker.get_individual_points(tdo, seeding_criteria)
 
     individual_total_points =
       individual_results_details
@@ -241,192 +240,6 @@ defmodule AptaSeeding.SeedingManager do
       result = is_current_tournament(tournament, target_tournaments)
     end)
     |> Enum.count()
-  end
-
-  #
-  # Get Individual Points
-  #
-
-  def get_individual_points(
-        team_data_object,
-        :team_has_played_2_tournaments_1_best_individual = seeding_criteria
-      ) do
-    get_individual_points(team_data_object, seeding_criteria, 1)
-  end
-
-  def get_individual_points(
-        team_data_object,
-        :team_has_played_1_tournament_2_best_individual = seeding_criteria
-      ) do
-    get_individual_points(team_data_object, seeding_criteria, 2)
-  end
-
-  def get_individual_points(
-        team_data_object,
-        :team_has_not_played_together_3_best_individual = seeding_criteria
-      ) do
-    get_individual_points(team_data_object, seeding_criteria, 3)
-  end
-
-  @doc """
-  This is where the logic for getting individual results goes
-  """
-  def get_individual_points(team_data_object, _seeding_criteria, tournaments_to_take) do
-    player_1_results =
-      get_highest_individual_results_for_player(team_data_object.player_1, tournaments_to_take)
-
-    player_2_results =
-      get_highest_individual_results_for_player(team_data_object.player_2, tournaments_to_take)
-
-    [player_1_results, player_2_results]
-    |> Enum.sort_by(fn r -> r.total_points end)
-    |> Enum.reverse()
-  end
-
-  def get_highest_individual_results_for_player(player, tournaments_to_take) do
-    team_results_objects =
-      player.individual_results
-      |> Enum.sort_by(fn id ->
-        id =
-          id
-          |> Data.preload_tournament()
-
-        d = id.tournament.date
-        {d.year, d.month, d.day}
-      end)
-      |> Enum.map(fn id ->
-        id
-        |> Data.preload_tournament()
-        |> Data.preload_player()
-      end)
-      |> Enum.reverse()
-      |> Enum.map(fn id ->
-        create_result_data_structure(id, :individual)
-      end)
-      |> Enum.take(tournaments_to_take)
-
-    total_points = calculate_total_points(team_results_objects)
-
-    %{total_points: total_points, details: team_results_objects}
-  end
-
-  def create_result_data_structure(tr, result_type) do
-    target_tournaments =
-      Enum.filter(Data.list_tournaments(), fn t -> t.name == tr.tournament.name end)
-
-    # get the multiplier for a tournament
-    result = get_tournament_multiplier(tr.tournament, target_tournaments, result_type)
-
-    %{
-      tournament_unique_name: tr.tournament.name_and_date_unique_name,
-      player: tr.player.name,
-      multiplier: result.multiplier,
-      points: tr.points,
-      total_points: Decimal.mult(result.multiplier, tr.points),
-    }
-  end
-
-  def calculate_total_points(results) do
-    results
-    |> Enum.reduce(0, fn obj, acc ->
-      Decimal.add(acc, obj.total_points)
-    end)
-  end
-
-  #
-  # Get Team Points
-  #
-
-  def get_team_points(team_data_object, _seeding_criteria, tournaments_to_take) do
-    team_results_objects =
-      team_data_object.team.team_results
-      |> Enum.sort_by(fn tr ->
-        tr =
-          tr
-          |> Data.preload_tournament()
-
-        d = tr.tournament.date
-        {d.year, d.month, d.day}
-      end)
-      |> Enum.map(fn tr -> Data.preload_tournament(tr) end)
-      |> Enum.reverse()
-      |> Enum.take(tournaments_to_take)
-      |> Enum.map(fn tr ->
-        target_tournaments =
-          Enum.filter(Data.list_tournaments(), fn t -> t.name == tr.tournament.name end)
-
-        result = get_tournament_multiplier(tr.tournament, target_tournaments, :team)
-
-        %{
-          tournament_unique_name: tr.tournament.name_and_date_unique_name,
-          team: tr.team.name,
-          multiplier: result.multiplier,
-          points: tr.points,
-          total_points: Decimal.mult(result.multiplier, tr.points)
-        }
-      end)
-
-    total_points = calculate_total_points(team_results_objects)
-    %{total_points: total_points, details: team_results_objects}
-  end
-
-  def get_team_points(team_data_object, :team_has_played_3_tournaments = seeding_criteria) do
-    get_team_points(team_data_object, seeding_criteria, 3)
-  end
-
-  def get_team_points(
-        team_data_object,
-        :team_has_played_2_tournaments_1_best_individual = seeding_criteria
-      ) do
-    get_team_points(team_data_object, seeding_criteria, 2)
-  end
-
-  def get_team_points(
-        team_data_object,
-        :team_has_played_1_tournament_2_best_individual = seeding_criteria
-      ) do
-    get_team_points(team_data_object, seeding_criteria, 1)
-  end
-
-  #
-  # Multiplier
-  #
-
-  @doc """
-  We get the multiplier for each tournament.
-  The trick is to know what a "current tournament" is, which not even the APTA seems
-  to know exactly, at least not how they state on their rules. From our side, a
-  current tournament is a tournament that has not been played in the current season.
-
-  Ex:
-  We are currently in Sep 2018.
-    Charities 2017 was played in Nov 2017. This is a "current tournament", should be 100% of points, multiplier 1
-      (Note that Charities 2018 will happen in Nov 2018, so not yet in this example.)
-    Charities 2016 was played in Nov 2016. This is not a current tournament, should be 90%, because it was 1 season ago. multiplier 0.9
-    Charities 2015 was played in Nov 2015. This is not a current tournament, should be 50%, because it was 2 seasons ago. multiplier 0.5
-  """
-  def get_tournament_multiplier(tournament, all_tournaments, type) do
-      result = create_tournament_multiplier_matrix(tournament, all_tournaments, type)
-
-        shim = result
-        |> Enum.find(fn {t, _multiplier} ->
-           t.name_and_date_unique_name == tournament.name_and_date_unique_name
-         end)
-
-     {t, multiplier} =
-       case shim do
-         nil ->
-           require IEx; IEx.pry
-
-         {t, multiplier} ->
-           {t, multiplier}
-       end
-
-    %{tournament: t, multiplier: multiplier}
-  end
-
-  def create_tournament_multiplier_matrix(tournament, all_tournaments, type) do
-    SeasonManager.create_tournament_multiplier_matrix(tournament, all_tournaments, type)
   end
 
   def is_current_tournament(tournament, all_tournaments) do
--- test/apta_seeding/seeding_manager/season_manager_test.exs
+++ test/apta_seeding/seeding_manager/season_manager_test.exs
@@ -2,6 +2,31 @@ defmodule AptaSeeding.SeedingManager.SeasonManagerTest do
   use ExUnit.Case
 
   alias AptaSeeding.SeedingManager.SeasonManager
+  alias AptaSeeding.Data
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(AptaSeeding.Repo)
+  end
+
+  def create_charities_2017_2016() do
+    [
+      %{
+        name: "Chicago Charities Men",
+        name_and_date_unique_name: "Chicago Charities Men|2017-11-04",
+        date: ~D[2017-11-04],
+        results_have_been_processed: true,
+        raw_results_html: "html"
+      },
+      %{
+        name: "Chicago Charities Men",
+        name_and_date_unique_name: "Chicago Charities Men|2016-11-05",
+        date: ~D[2016-11-05],
+        results_have_been_processed: true,
+        raw_results_html: "html"
+      }
+    ]
+    |> Enum.map(fn tournament_attrs -> Data.create_tournament(tournament_attrs) end)
+  end
 
   describe "seasons_ago/1" do
     test "returns the correct number of seasons ago - current season" do
@@ -42,4 +67,37 @@ defmodule AptaSeeding.SeedingManager.SeasonManagerTest do
       assert result.multiplier == Decimal.new("0.5")
     end
   end
+
+  describe "create_tournament_multiplier_matrix/3" do
+
+    test "returns a matrix with the correct multipliers" do
+      create_charities_2017_2016()
+
+      charities_2017 =
+        Data.list_tournaments()
+        |> Enum.find(fn t -> t.name_and_date_unique_name == "Chicago Charities Men|2017-11-04" end)
+
+      result =
+        SeasonManager.create_tournament_multiplier_matrix(
+          charities_2017,
+          Data.list_tournaments(),
+          :team
+        )
+
+      {most_recent_tournament, most_recent_multiplier} = Enum.at(result, 0)
+
+      assert most_recent_tournament.name_and_date_unique_name ==
+               "Chicago Charities Men|2017-11-04"
+
+      assert most_recent_multiplier == Decimal.new(1.0)
+
+      {second_most_recent_tournament, second_most_recent_multiplier} = Enum.at(result, 1)
+
+      assert second_most_recent_tournament.name_and_date_unique_name ==
+               "Chicago Charities Men|2016-11-05"
+
+      assert second_most_recent_multiplier == Decimal.new(0.9)
+    end
+  end
+
 end
