3f5902b2c96cb4c4d7fc9bb182e02d921c8555d3
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-09-26 18:31:50 -0500

    See debug logs in test

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ config :apta_seeding, AptaSeedingWeb.Endpoint,
   server: false
 
 # Print only warnings and errors during test
-config :logger, level: :warn
+config :logger, level: :debug
 
 # Configure your database
 config :apta_seeding, AptaSeeding.Repo,
